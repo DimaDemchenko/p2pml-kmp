590cfe95e72863936b0fc4a6bbbf96d9e9a23a9b
DimaDemchenko <demchenkodmytro73@gmail.com>
2025-11-05 21:54:07 +0200

    feat: implement CoreEventMap and EventEmitter for event handling in P2P media loader

---
+++ shared/src/commonMain/kotlin/com/novage/p2pml/eventEmitter/EventEmitter.kt
@@ -0,0 +1,28 @@
+package com.novage.p2pml.eventEmitter
+
+fun interface EventListener<T> {
+    fun onEvent(data: T)
+}
+
+class EventEmitter {
+    private val listeners = mutableMapOf<CoreEventMap<*>, MutableList<EventListener<*>>>()
+
+    fun <T> addEventListener(event: CoreEventMap<T>, listener: EventListener<T>) {
+        val list = listeners.getOrPut(event) { mutableListOf() }
+        list.add(listener)
+    }
+
+    fun <T> removeEventListener(event: CoreEventMap<T>, listener: EventListener<T>) {
+        listeners[event]?.remove(listener)
+    }
+
+    fun <T> emit(event: CoreEventMap<T>, data: T) {
+        listeners[event]?.forEach {
+            @Suppress("UNCHECKED_CAST") (it as EventListener<T>).onEvent(data)
+        }
+    }
+
+    fun removeAllListeners() {
+        listeners.clear()
+    }
+}
