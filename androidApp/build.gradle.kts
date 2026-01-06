e696b6c4be6ce5ccb1be1990da7c39fbec9fef9e
DimaDemchenko <demchenkodmytro73@gmail.com>
2026-01-06 07:03:45 +0200

    refactor: remove unused test dependencies from build.gradle.kts

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -22,8 +22,6 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -50,7 +48,6 @@ dependencies {
     implementation(libs.androidx.media3.ui)
     implementation(libs.androidx.compose.material.icons.extended)
 
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -59,12 +56,4 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.material3)
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
