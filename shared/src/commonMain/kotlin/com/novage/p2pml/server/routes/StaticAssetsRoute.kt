625b08ce1ab9343269a2e0f5f7bc695cc49eb224
DimaDemchenko <demchenkodmytro73@gmail.com>
2026-01-06 08:11:36 +0200

    refactor: integrate asset generation task and update static asset handling

--- shared/src/commonMain/kotlin/com/novage/p2pml/server/routes/StaticAssetsRoute.kt
+++ shared/src/commonMain/kotlin/com/novage/p2pml/server/routes/StaticAssetsRoute.kt
@@ -1,30 +1,33 @@
 package com.novage.p2pml.server.routes
 
+import com.novage.p2pml.shared.generated.P2PAssets
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondBytes
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
-import p2pmedialoadermobile.shared.generated.resources.Res
-import kotlin.text.ifEmpty
+import io.ktor.util.decodeBase64Bytes
 
 internal fun Route.registerWebAssets() {
     get("/${RoutePaths.STATIC}/{path...}") {
         val path = call.parameters.getAll("path")?.joinToString("/")?.ifEmpty { null }
             ?: "index.html"
 
-        try {
-            val bytes = Res.readBytes("files/$path")
+        val bytes = when (path) {
+            "index.html" -> P2PAssets.INDEX_HTML_BASE64.decodeBase64Bytes()
+            P2PAssets.JS_FILENAME -> P2PAssets.CORE_JS_BASE64.decodeBase64Bytes()
+            else -> null
+        }
 
+        if (bytes != null) {
             val contentType = when {
                 path.endsWith(".js") -> ContentType.Application.JavaScript
                 path.endsWith(".html") -> ContentType.Text.Html
                 else -> ContentType.Application.OctetStream
             }
-
             call.respondBytes(bytes, contentType)
-        } catch (_: Exception) {
+        } else {
             call.respond(HttpStatusCode.NotFound)
         }
     }
