a67df8016e8c50a76d5b0512cb0b5f82d6a818f0
DimaDemchenko <demchenkodmytro73@gmail.com>
2026-03-13 22:08:43 +0200

    refactor: remove unused Kotlin Android plugin references from Gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
 
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
 
     alias(libs.plugins.spotless) apply false
     alias(libs.plugins.detekt) apply false
