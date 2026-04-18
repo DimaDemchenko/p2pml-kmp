813e9d4c172a140afc1e43f3e79290368c75a4ad
DimaDemchenko <demchenkodmytro73@gmail.com>
2026-04-18 13:53:55 +0300

    refactor: move Java interop files to appropriate directory

--- p2pml/src/androidMain/kotlin/com/novage/p2pml/api/interop/P2PMediaLoaderJava.kt
+++ p2pml/src/androidMain/kotlin/com/novage/p2pml/api/interop/P2PMediaLoaderJava.kt
@@ -1,6 +1,7 @@
-package com.novage.p2pml
+package com.novage.p2pml.api.interop
 
 import androidx.media3.exoplayer.ExoPlayer
+import com.novage.p2pml.P2PMediaLoader
 import com.novage.p2pml.api.models.DynamicCoreConfig
 import com.novage.p2pml.api.models.PlaybackInfo
 import java.lang.AutoCloseable
