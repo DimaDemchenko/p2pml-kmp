ba6394910bd436a312a3532ef750c66542ff2277
DimaDemchenko <demchenkodmytro73@gmail.com>
2026-06-05 15:29:46 +0300

    refactor: simplify formatting of createHeadlessWebView signature in WebViewFactory

--- p2pml/src/commonMain/kotlin/com/novage/p2pml/internal/webview/WebViewFactory.kt
+++ p2pml/src/commonMain/kotlin/com/novage/p2pml/internal/webview/WebViewFactory.kt
@@ -4,8 +4,5 @@ import com.novage.p2pml.api.errors.P2PMediaLoaderException
 import com.novage.p2pml.api.events.P2PEvents
 
 internal interface WebViewFactory {
-    fun createHeadlessWebView(
-        events: P2PEvents,
-        onFatalError: (P2PMediaLoaderException) -> Unit
-    ): HeadlessWebView
+    fun createHeadlessWebView(events: P2PEvents, onFatalError: (P2PMediaLoaderException) -> Unit): HeadlessWebView
 }
