0a0a9e857df29d2e48a9e28fb05f8aaa1b4a136c
DimaDemchenko <demchenkodmytro73@gmail.com>
2026-07-08 21:47:25 +0300

    chore: satisfy PR lint checks

--- iosDemo/iosDemo/UI/Components/ShareSheet.swift
+++ iosDemo/iosDemo/UI/Components/ShareSheet.swift
@@ -4,9 +4,9 @@ import UIKit
 struct ShareSheet: UIViewControllerRepresentable {
     let activityItems: [Any]
 
-    func makeUIViewController(context: Context) -> UIActivityViewController {
+    func makeUIViewController(context _: Context) -> UIActivityViewController {
         UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
     }
 
-    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
+    func updateUIViewController(_: UIActivityViewController, context _: Context) {}
 }
