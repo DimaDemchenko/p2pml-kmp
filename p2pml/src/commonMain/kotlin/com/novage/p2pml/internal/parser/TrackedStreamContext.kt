54bf7fb5b81b5c800d0787bf8f77fcd9b0c19175
DimaDemchenko <demchenkodmytro73@gmail.com>
2026-07-15 18:38:15 +0300

    fix: require an explicit time mark in TrackedStreamContext
    The TimeSource.Monotonic default was dead (the only construction site
    always passes the tracker's injected clock) and a testability trap: a
    future call site relying on it would silently escape fake time sources
    and make TTL eviction untestable.

--- p2pml/src/commonMain/kotlin/com/novage/p2pml/internal/parser/TrackedStreamContext.kt
+++ p2pml/src/commonMain/kotlin/com/novage/p2pml/internal/parser/TrackedStreamContext.kt
@@ -4,12 +4,11 @@ import com.novage.p2pml.api.events.Segment
 import com.novage.p2pml.internal.parser.hlsPlaylistParser.Stream
 import com.novage.p2pml.internal.parser.hlsPlaylistParser.UpdateStreamParams
 import kotlin.time.TimeMark
-import kotlin.time.TimeSource
 
 internal class TrackedStreamContext(
     var stream: Stream,
     val segments: MutableMap<Long, Segment> = mutableMapOf(),
     val currentSegmentRuntimeIds: MutableSet<String> = mutableSetOf(),
     var updateParams: UpdateStreamParams? = null,
-    var lastUpdated: TimeMark = TimeSource.Monotonic.markNow()
+    var lastUpdated: TimeMark
 )
